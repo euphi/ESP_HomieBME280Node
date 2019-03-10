fd6fffe697aac1367f0f211f33dfafc8b906b99a
Ian Hubbertz <ian_web1@hubbertz.de>
2019-03-10 17:11:57 +0100

    Updates (dewpoint, library configuration, ESP32 build)

--- src/HomieBME280Node.cpp
+++ src/HomieBME280Node.cpp
@@ -14,6 +14,8 @@ HomieBME280Node::HomieBME280Node():
 	advertise("temperature").setName("Temperatur").setDatatype("float").setUnit("°C");
 	advertise("pressure").setName("Luftdruck").setDatatype("float").setUnit("hPa");
 	advertise("humidity").setName("Luftfeuchtigkeit").setDatatype("float").setUnit("%");
+	advertise("dewPoint").setName("Taupunkt").setDatatype("float").setUnit("°C");
+
 }
 
 void HomieBME280Node::setup() {
@@ -21,45 +23,45 @@ void HomieBME280Node::setup() {
 	sensor.settings.I2CAddress = 0x76;
 
 	//  0, Sleep mode
-		//  1 or 2, Forced mode
-		//  3, Normal mode
-		sensor.settings.runMode = 3; //Forced mode
+	//  1 or 2, Forced mode
+	//  3, Normal mode
+	sensor.settings.runMode = 3; //Forced mode
 
-		//tStandby can be:
-		//  0, 0.5ms
-		//  1, 62.5ms
-		//  2, 125ms
-		//  3, 250ms
-		//  4, 500ms
-		//  5, 1000ms
-		//  6, 10ms
-		//  7, 20ms
-		sensor.settings.tStandby = 0;
+	//tStandby can be:
+	//  0, 0.5ms
+	//  1, 62.5ms
+	//  2, 125ms
+	//  3, 250ms
+	//  4, 500ms
+	//  5, 1000ms
+	//  6, 10ms
+	//  7, 20ms
+	sensor.settings.tStandby = 0;
 
-		//filter can be off or number of FIR coefficients to use:
-		//  0, filter off
-		//  1, coefficients = 2
-		//  2, coefficients = 4
-		//  3, coefficients = 8
-		//  4, coefficients = 16
-		sensor.settings.filter = 0;
+	//filter can be off or number of FIR coefficients to use:
+	//  0, filter off
+	//  1, coefficients = 2
+	//  2, coefficients = 4
+	//  3, coefficients = 8
+	//  4, coefficients = 16
+	sensor.settings.filter = 0;
 
-		//tempOverSample can be:
-		//  0, skipped
-		//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
-		sensor.settings.tempOverSample = 1;
+	//tempOverSample can be:
+	//  0, skipped
+	//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+	sensor.settings.tempOverSample = 1;
 
-		//pressOverSample can be:
-		//  0, skipped
-		//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
-	    sensor.settings.pressOverSample = 1;
+	//pressOverSample can be:
+	//  0, skipped
+	//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+	sensor.settings.pressOverSample = 1;
 
-		//humidOverSample can be:
-		//  0, skipped
-		//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
-		sensor.settings.humidOverSample = 1;
-		Wire.begin(SDA, SCL);
-		Serial.println(sensor.begin(), HEX);
+	//humidOverSample can be:
+	//  0, skipped
+	//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+	sensor.settings.humidOverSample = 1;
+	Wire.begin(SDA, SCL);
+	Serial.println(sensor.begin(), HEX);
 }
 
 void HomieBME280Node::loop() {
@@ -68,12 +70,14 @@ void HomieBME280Node::loop() {
 	if (now > nextRead) {
 		nextRead = now + 60000;
 		curTemp = sensor.readTempC();
-		curPres = sensor.readFloatPressure();
+		curPres = sensor.readFloatPressure()/100;
 		curHum =  sensor.readFloatHumidity();
+		float dewPoint = sensor.dewPointC();
 		Serial.printf("Read values:\n\tPressure: %f mbar\n\tTemperatur: %f °C\n\tHumidity:%f relH\n", curPres, curTemp, curHum);
 		setProperty("temperature").send(String(curTemp));
-		setProperty("pressure").send(String(curPres/100));
+		setProperty("pressure").send(String(curPres));
 		setProperty("humidity").send(String(curHum));
+		setProperty("dewPoint").send(String(dewPoint));
 	}
 }
 
